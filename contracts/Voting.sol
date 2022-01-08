df48842f28d11f690f6e7609c9abb1d8016dac0f
hemang <hemangahujaeotw@gmail.com>
2022-01-08 23:52:40 +0530

    reset and state functionalities added

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -9,17 +9,30 @@ contract Voting{
     address[] private candidates;
     address private Winner;
     address public owner;
+    enum State {
+        inProgress,
+        Ended
+    }
+    State public state;
     constructor(){
         owner = msg.sender; 
+        Winner = address(0);
+        state = State.inProgress;
     }
     mapping(address => uint) private votes;
 
     function addCandidate(address candidate) public {
+        require(state == State.inProgress, "Voting is not in progress");
         require(msg.sender == owner , "Only owner can add candidates");
+        require(candidate != address(0), "Candidate cannot be null");
+        require(candidateExists(candidate) == false, "Candidate already exists");
         candidates.push(candidate);
     }
     function getLength() public view returns (uint256){
         return candidates.length;
+    }
+    function getCandidate(uint256 index) public view returns (address){
+        return candidates[index];
     }
     function candidateExists(address candidate) internal view returns (bool){
         for(uint i = 0; i < candidates.length; i++){
@@ -37,9 +50,11 @@ contract Voting{
     function getVotes(address candidate) public view returns (uint256){
         return votes[candidate];
     }
-    function getWinner() public view returns (address){
-        require(msg.sender == owner , "Only owner can declare winner");
+    function calculateWinner() public {
+        require(state == State.inProgress, "Voting has ended or is not in progress");
+        require(msg.sender == owner , "Only owner can calculate winner");
         require(candidates.length > 0 , "No candidates added yet");
+        state = State.Ended;
         address winner = candidates[0];
         uint winnerVotes = 0;
         for (uint i = 0; i < candidates.length; i++) {
@@ -50,6 +65,21 @@ contract Voting{
                 winnerVotes = count;
             }
         }
-        return winner;
+        Winner = winner;
+    }
+    function getWinner() public view returns (address){
+        require(state == State.Ended,"Winner not calculated yet");
+        return Winner;
+    }
+    function reset() public{
+        require(msg.sender == owner , "Only owner can reset");
+        for(uint i = 0; i < candidates.length; i++){
+            votes[candidates[i]] = 0;
+        }
+        while(candidates.length > 0){
+            candidates.pop();
+        }
+        Winner = address(0);
+        state = State.inProgress;
     }
 }
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,15 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const address1 = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+const address2 = "0x782D8027c193c75f3DcC62A7bAe2D4C5a7ff71A2"
+
+
+
 describe('Voting', function () {
+
+  
+
   before(async function () {
     this.Voting = await ethers.getContractFactory('Voting');
   });
@@ -11,26 +19,68 @@ describe('Voting', function () {
     await this.voting.deployed();
   });
 
-
+  it('should return state to be in progress', async function () {
+    const state = await this.voting.state();
+    expect(state).to.equal(0);
+  }
+  )
   it('add candidate', async function () {
     
-    await this.voting.addCandidate("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+    await this.voting.addCandidate(address1);
     expect(await this.voting.getLength()).to.equal(1);
   });
 
+  it('should return address of added candidate' , async function(){
+    await this.voting.addCandidate(address1);
+    await this.voting.addCandidate(address2);
+    expect(await this.voting.getCandidate(1)).to.equal(address2);
+  })
   it('vote for candidate without adding should fail' , async function () {
-    expect(this.voting.voteForCandidate("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")).to.be.revertedWith("Candidate does not exist");
+    await expect(this.voting.voteForCandidate(address1)).to.be.revertedWith("Candidate does not exist");
     
   })
 
   it('test winning' , async function(){
-    await this.voting.addCandidate("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-    await this.voting.addCandidate("0x782D8027c193c75f3DcC62A7bAe2D4C5a7ff71A2");
-    await this.voting.voteForCandidate("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-    await this.voting.voteForCandidate("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-    await this.voting.voteForCandidate("0x782D8027c193c75f3DcC62A7bAe2D4C5a7ff71A2");
-    await this.voting.voteForCandidate("0x782D8027c193c75f3DcC62A7bAe2D4C5a7ff71A2");
-    await this.voting.voteForCandidate("0x782D8027c193c75f3DcC62A7bAe2D4C5a7ff71A2");
-    expect(await this.voting.getWinner()).to.equal("0x782D8027c193c75f3DcC62A7bAe2D4C5a7ff71A2");
+    await this.voting.addCandidate(address1);
+    await this.voting.addCandidate(address2);
+    await this.voting.voteForCandidate(address1);
+    await this.voting.voteForCandidate(address1);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.calculateWinner();
+    expect(await this.voting.getWinner()).to.equal(address2);
+  })
+  it('test reset' , async function(){
+    await this.voting.addCandidate(address1);
+    await this.voting.addCandidate(address2);
+    await this.voting.voteForCandidate(address1);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.calculateWinner();
+    expect(await this.voting.getWinner()).to.equal(address2);
+    await this.voting.reset();
+    expect(await this.voting.getLength()).to.equal(0);
+    await expect(this.voting.calculateWinner()).to.be.revertedWith('No candidates added yet');
+  })
+  it('test null winner' , async function(){
+    await expect(this.voting.getWinner()).to.be.revertedWith('Winner not calculated yet');
+  })
+  it('should return state to be ended after calculating winner' , async function(){
+    await this.voting.addCandidate(address1);
+    await this.voting.addCandidate(address2);
+    await this.voting.voteForCandidate(address1);
+    await this.voting.voteForCandidate(address1);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.voteForCandidate(address2);
+    await this.voting.calculateWinner();
+    expect(await this.voting.state()).to.equal(1);
+  })
+  it('test non owner accessibility' , async function(){
+    const [owner,other] = await ethers.getSigners();
+    await expect(this.voting.connect(other).addCandidate(address1)).to.be.revertedWith("Only owner can add candidates");
+    await expect(this.voting.connect(other).calculateWinner()).to.be.revertedWith("Only owner can calculate winner");
+    await expect(this.voting.connect(other).reset()).to.be.revertedWith("Only owner can reset");
   })
 });
